1fda741675889f79286b4644710371277a19c550
Marcus Tutorials <marcusharting@web.de>
2022-12-07 13:38:04 +0100

    Update Dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: ''
+  publicPath: '',
+  configureWebpack: {
+  //   plugins: [new BundleAnalyzerPlugin()],
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'all',
+          },
+        }
+      }
+    }
+  }
 })
